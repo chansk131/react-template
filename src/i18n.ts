3cf9c8db61846aa4f446fa5aa68420311c794c32
Theerapat Muangpoon <chansk131@gmail.com>
2020-11-09 15:15:39 +0700

    add i18n

---
+++ src/i18n.ts
@@ -0,0 +1,25 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+
+import Backend from 'i18next-http-backend'
+import LanguageDetector from 'i18next-browser-languagedetector'
+
+i18n
+  // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
+  // learn more: https://github.com/i18next/i18next-http-backend
+  .use(Backend)
+  // detect user language
+  // learn more: https://github.com/i18next/i18next-browser-languageDetector
+  .use(LanguageDetector)
+  // pass the i18n instance to react-i18next.
+  .use(initReactI18next)
+  .init({
+    lng: 'en',
+    fallbackLng: 'en',
+
+    interpolation: {
+      escapeValue: false, // react already safes from xss
+    },
+  })
+
+export default i18n
